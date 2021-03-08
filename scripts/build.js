088d15e8706638f843397300d0c5d152d2b2fad2
Patrick <patrick@permafrost.dev>
2021-03-08 18:31:40 -0500

    add build script

---
+++ scripts/build.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const path = require('path');
+const projectPath = path.dirname(__dirname);
+const pkg = require(projectPath + '/package.json');
+
+const filenames = {
+    input: projectPath + '/src/index.ts',
+    output: projectPath + pkg.main,
+};
+
+class ProjectBuilder {
+    build() {
+        require('esbuild').buildSync({
+            entryPoints: [filenames.input],
+            bundle: true,
+            platform: 'node',
+            target: 'node14',
+            format: 'cjs',
+            outfile: filenames.output,
+            external: ['axios', 'fastify', 'fs'],
+            define: {
+                __BUILD_VERSION__: '"' + pkg.version + '"',
+            },
+        });
+
+        this.replaceConstants();
+    }
+
+    replaceConstants() {
+        try {
+            let contents = fs.readFileSync(filenames.output, { encoding: 'utf-8' }).toString();
+            contents = contents.replace(/__BUILD_VERSION__/g, pkg.version);
+            contents = contents.replace(/__APP_NAME__/g, pkg.name);
+
+            fs.writeFileSync(filenames.output, contents);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
+
+function main() {
+    console.log('Building project... (' + filenames.input.replace(projectPath, '.') + ')');
+
+    const builder = new ProjectBuilder();
+
+    builder.build();
+
+    console.log('Build complete: ' + filenames.output.replace(projectPath, '.'));
+}
+
+main();

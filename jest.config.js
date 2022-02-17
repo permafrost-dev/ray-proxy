3b3f5fa9d543b0fc4ed662fbf2aaf54d7b591f91
Patrick Organ <patrick@permafrost.dev>
2022-02-17 17:30:49 -0500

    add path mappings

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+
+const tsConfigPaths = {
+    '@/*': ['src/*'],
+    '@tests/*': ['tests/*'],
+};
+
 module.exports = {
     preset: 'ts-jest/presets/js-with-ts',
     testEnvironment: 'node',
@@ -7,6 +14,7 @@ module.exports = {
     testRegex: '(/__test__/.*|/tests/.*|(\\.|/)(test|spec))\\.[tj]sx?$',
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+    moduleNameMapper: pathsToModuleNameMapper(tsConfigPaths, { prefix: `${__dirname}/` }),
 
     coverageDirectory: './coverage',
     coverageReporters: ['html', 'text'],
